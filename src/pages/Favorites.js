795ef1f95d6a9df45977fa7813931c1083dd5652
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2021-07-05 23:17:23 -0400

    Meetups-Reactjs

---
+++ src/pages/Favorites.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
+
+function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) content = <p>You got no favorite Meetup spots. Add some</p>
+  else content = <MeetupList meetups={favoritesCtx.favorites} />
+  
+  return <section>
+    <h1>My Favorites</h1>
+    {content}
+  </section>;
+}
+
+export default FavoritesPage;
